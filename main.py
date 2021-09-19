f610bf3ac85e914902ee3775dd91fd182a820a36
syzcoycosuvsgix <89813564+syzcoycosuvsgix@users.noreply.github.com>
2021-09-19 08:06:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import multi, configparser, json
-from multiprocess import Process
+from multiprocess import Process as GO
 
 config = configparser.ConfigParser()
 config.read("config.ini")
@@ -12,12 +12,12 @@ print("total accounts> %d \n" % len(tokens))
 input("1) flood to chat\n\n>> ")
 
 for token in tokens:
-    multi.Client(
+    GO(multi.Client(
     session_name=token,
     api_id=api_id,
     api_hash=api_hash,
     bot_token=token,
-    messages=text).start()
+    messages=text).start())
     print(f"Log as {token[:5]}")
 print("send <a> to chat")
 pyrogram.idle()
