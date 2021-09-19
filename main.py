987ce7ce2974095ff10186d8a209c0f47cc6cf13
syzcoycosuvsgix <89813564+syzcoycosuvsgix@users.noreply.github.com>
2021-09-20 01:08:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from pyrogram import Client, idle
 import sys
 
-app = Client("diamond-userbot")
+app = Client("diamond")
 
 if __name__ == "__main__":
     app.start()
     if len(sys.argv) == 4:
         try:
             type = sys.argv[3]
-            if type == "update": text = "Обновление установлено успешно!"
-            else: text = "Перезагрузка прошла успешно!"
+            if type == "update": text = "**Обновление установлено успешно!**"
+            else: text = "**Перезагрузка прошла успешно!**"
             app.send_message(
                 chat_id=sys.argv[1], text=text, reply_to_message_id=int(sys.argv[2])
             )
