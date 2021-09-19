86594c9dccb41dd916814c1b08f444fa75f10d97
syzcoycosuvsgix <89813564+syzcoycosuvsgix@users.noreply.github.com>
2021-09-19 08:37:16 +0300

    Update multi.py

--- multi.py
+++ multi.py
@@ -1,16 +1,4 @@
 import pyrogram, random
 
 def Client(session_name, api_id, api_hash, bot_token, messages):
-    bot = pyrogram.Client(session_name, api_id, api_hash, bot_token=bot_token)
-    @bot.on_message()
-    async def o(client, message):
-        if message.text != "a":
-            return
-        me = await bot.get_me()
-        while True:
-            try:
-                await client.send_message(message.chat.id, random.choice(messages))
-                print(f"[{me.username}] sended.")
-            except:
-                print(f"[{me.username}] not sended.")
-    bot.run()
+    return pyrogram.Client(session_name, api_id, api_hash, bot_token=bot_token)
