57405b580981ec21767a49fe6d388685f2b69075
syzcoycosuvsgix <89813564+syzcoycosuvsgix@users.noreply.github.com>
2021-09-19 08:11:40 +0300

    Update multi.py

--- multi.py
+++ multi.py
@@ -1,3 +1,11 @@
-import pyrogram
+import pyrogram, random
 
-def Client(session_name, api_id, api_hash, bot_token, 
+def Client(session_name, api_id, api_hash, bot_token, messages):
+    bot = pyrogram.Client(session_name, api_id, api_hash, bot_token=bot_token)
+    @bot.on_message()
+    async def o(client, message):
+        if message.text != "a":
+            return
+        while True:
+            await client.send_message(message.chat.id, random.choice(messages))
+    bot.run()
