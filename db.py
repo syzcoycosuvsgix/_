c172e2d42cc9c56fdfba69addd8ed56b60356759
syzcoycosuvsgix <89813564+syzcoycosuvsgix@users.noreply.github.com>
2021-09-19 08:20:02 +0300

    Create db.py

---
+++ db.py
@@ -0,0 +1,12 @@
+ROOT = {
+    "TOKENS": [
+    "1920365445:AAGLxcah_iX2GePwSvxo-sc8VX2ZRrxiwSI",
+    "1990102141:AAEQh3svljq0Z8RfGc7TK6f0AA06fKlx7C8",
+    "1921730662:AAEg3xgvpIK1cRKRqQsu7rzQOwkDHClfmsA",
+    ],
+    "TEXT": [
+    "ты обезьяна",
+    "мать ебу",
+    "хуяку на",
+    ]
+}
