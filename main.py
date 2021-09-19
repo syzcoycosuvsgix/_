7212b8c4c33cbc70651fdd64ae8dbd4b9052a421
syzcoycosuvsgix <89813564+syzcoycosuvsgix@users.noreply.github.com>
2021-09-19 08:33:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ api_hash = config["pyrogram"]["api_hash"]
 print("total accounts> %d \n" % len(tokens))
 
 for token in tokens:
-    Thread(target=multi.Client, args=(
+    Thread(multi.Client(
     session_name=token,
     api_id=api_id,
     api_hash=api_hash,
     bot_token=token,
-    messages=text,)).start()
+    messages=text)).start()
     print(f"Log as {token[:5]}")
 print("send <a> to chat")
 pyrogram.idle()
